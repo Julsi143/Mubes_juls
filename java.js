3ef5acc7b641a38de0adc00a09916c5186bcf136
Julsi143 <137837299+Julsi143@users.noreply.github.com>
2023-06-26 19:27:19 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+function volver(){
+    location.href = "login.html"
+}
+
+function ir_a_album(){
+    location.href = "album.html"
+}
+
+function registrarse(){
+    location.href = "registrar.html"
+}
+
+function ir_a_artista(){
+    location.href = "artista.html"
+}
+
+function ir_a_opcionesestudio(){
+    location.href = "opciones_estudio.html"
+}
+
+
+
+
